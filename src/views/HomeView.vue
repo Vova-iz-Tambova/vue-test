5807079b5162c41441d43b16b6b7923bd64075bc
Vova iz Tambova <voller.ruden@yandex.ru>
2024-02-15 12:06:22 +0300

    add vue-validation tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import type CatalogVue from '@/components/catalog/Catalog.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <CatalogVue />
   </main>
 </template>
